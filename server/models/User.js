f822ccc415c1571f3776eb284cd49d960b4e624b
pdvega <pdvegaa@gmail.com>
2017-09-14 13:19:56 +0700

    add models user

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema ({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'Username is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  }
+}, {
+  timestamps: true
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
