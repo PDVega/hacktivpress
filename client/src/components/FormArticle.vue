f9ad55a2915a4efbb7ad2b5864c56015db64d704
pdvega <pdvegaa@gmail.com>
2017-09-14 14:35:36 +0700

    add component

---
+++ client/src/components/FormArticle.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="submitArticle(formArticle)">
+    <div class="form-group">
+      <label class="control-label" for="focusedInput">Title</label>
+      <input class="form-control" type="text" v-model="formArticle.title">
+    </div>
+    <div class="form-group">
+      <label class="control-label" for="focusedInput">Content</label>
+      <input class="form-control" type="text" v-model="formArticle.content">
+    </div>
+    <div class="form-group">
+      <label class="control-label" for="focusedInput">Image</label>
+      <input class="form-control" type="text" v-model="formArticle.image">
+    </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="">
     <Navbar />
+    <div class="container">
+      <FormArticle />
+    </div>
   </div>
 </template>
 
 <script>
+import FormArticle from '@/components/FormArticle'
 import Navbar from '@/components/Navbar'
 export default {
   components: {
-    Navbar
+    Navbar,
+    FormArticle
   }
 }
 </script>
