5e65b64234093b9b1384349e038dff34c0b8ca42
pdvega <pdvegaa@gmail.com>
2017-09-14 15:10:01 +0700

    add new article done

--- client/src/components/FormArticle.vue
+++ client/src/components/FormArticle.vue
@@ -1,27 +1,53 @@
 <template>
-  <form @submit.prevent="submitArticle(formArticle)">
-    <div class="form-group">
-      <label class="control-label" for="focusedInput">Title</label>
-      <input class="form-control" type="text" v-model="formArticle.title">
+  <div id="#articleform" class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <form @submit.prevent="postArticle(formArticle)">
+          <div class="form-group">
+            <label class="control-label" for="focusedInput">Title</label>
+            <input class="form-control" type="text" v-model="formArticle.title">
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="focusedInput">Content</label>
+            <input class="form-control" type="text" v-model="formArticle.content">
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="focusedInput">Category</label>
+            <input class="form-control" type="text" v-model="formArticle.category">
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div class="form-group">
-      <label class="control-label" for="focusedInput">Content</label>
-      <input class="form-control" type="text" v-model="formArticle.content">
-    </div>
-    <div class="form-group">
-      <label class="control-label" for="focusedInput">Image</label>
-      <input class="form-control" type="text" v-model="formArticle.image">
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-  </form>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
+  data () {
+    return {
+      formArticle: {
+        title: '',
+        content: '',
+        category: '',
+        author: `${localStorage.getItem('id')}`
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'postArticle'
+    ])
+  }
 }
 </script>
 
 <style lang="css">
+#articleform {
+  text-align: center;
+  margin-top: 70px;
+}
 </style>
---
+++ client/src/vuex/store.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+Vue.use(Vuex)
+
+const state = {
+  articles: []
+}
+
+const mutations = {
+  setArticles (state, payload) {
+    state.articles = payload
+  },
+  saveArticle (state, payload) {
+    state.articles.push(payload)
+  }
+}
+
+const actions = {
+  getAllArticles ({ commit }) {
+    http.get('/articles')
+    .then(({data}) => {
+      commit('setArticles', data)
+    })
+    .catch(err => console.error(err))
+  },
+  postArticle ({ commit }, newArticle) {
+    http.post('/articles', newArticle)
+    .then(({data}) => {
+      commit('saveArticle', data)
+    })
+    .catch(err => console.error(err))
+  }
+}
+
+const store = new Vuex.Store({
+  state, mutations, actions
+})
+
+export default store
